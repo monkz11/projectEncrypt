9c4fd73b6cb9353cc8677fb53c3ded9d86b70ac5
Jikael Gagnon <jikaelgagnon@gmail.com>
2023-07-03 18:20:06 -0400

    Modified routes.py and HTML files - No longer saving user files, instead just reading directly from upload - Error handling for file upload added (eg. checks if file empty, no MRZ detected, etc) - Displays detected MRZ text and allows user to edit in case it made mistakes
    Next step is to setup database and create map from MRZ to unique identifier

--- encrypt/routes.py
+++ encrypt/routes.py
@@ -3,7 +3,11 @@ from werkzeug.utils import secure_filename
 import cv2
 import os
 from encrypt import app
-from encrypt.passport_scan import drawMRZRectangle
+from encrypt.passport_scan import drawMRZRectangle, getMRZCoords, getMRZText
+import numpy as np
+import io
+from PIL import Image
+import base64
 
 # Each page on our website is accessed through a route in the form "website/route"
 # For example youtube.com/home youtube.com/trending etc...
@@ -18,18 +22,44 @@ def home():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_passport():
-    # Checks if we tried to upload something
     if request.method == 'POST':
-        # passport_img is defined in the html file... check there for more
-        file = request.files['passport_img']
-        # function that cleans up filename. eg. "my file name" becomes "my_file_name"
-        filename = secure_filename(file.filename)  
-        upload_path = os.path.join('encrypt/static/uploads', filename)
-        scan_path = os.path.join('encrypt/static/scans', filename)
-        file.save(upload_path)
-        scan = drawMRZRectangle(cv2.imread(upload_path))
-        cv2.imwrite(scan_path, scan)
-        # Displays uploaded image on the page
-        return render_template('upload.html', title = "Upload", complete_scan=os.path.join('static/scans', filename))
-    # If nothing uploaded, render html without image
+        # Runs following branch if image is uploaded
+        if request.form.get('submit-button') == "img":
+            # passport_img is defined in the html file... check there for more
+            file = request.files.get('passport_img')
+            # If they submit with no file, just render normal page
+            if (not file):
+                return render_template('upload.html')
+            # Allows us to read image without saving it anywhere 
+            file_content = file.read()
+            # Convert to numpy array, read to cv2, and find mrz
+            img = np.asarray(bytearray(file_content), dtype="uint8")
+            img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+            # Get coords for mrz. If it fails, return type is None
+            try:
+                x,y,w,h = getMRZCoords(img)
+            except:
+                success_status = -1
+                return render_template('upload.html', title = "Upload", success_status = success_status)
+            success_status = 0
+            # If no failure, display the image
+            scan = cv2.rectangle(img, (x,y),(x+w, y+h),(0,255,0),2)
+            mrz = img[y:y + h, x:x + w]
+            mrz_text = getMRZText(mrz)
+            # Need to swap to RGB since cv2 uses BGR instead. If this is deleted, image colours r fucked
+            scan = cv2.cvtColor(scan, cv2.COLOR_BGR2RGB)
+            # Found on stackoverflow to display image without saving
+            img = Image.fromarray(scan.astype("uint8"))
+            rawBytes = io.BytesIO()
+            img.save(rawBytes, "JPEG")
+            rawBytes.seek(0)
+            img_base64 = base64.b64encode(rawBytes.getvalue()).decode('ascii')
+            mime = "image/jpeg"
+            uri = f"data:{mime};base64,{img_base64}"
+            return render_template('upload.html', title = "Upload", img_uri=uri, mrz_text = mrz_text, success_status = success_status)
+        if request.form.get('submit-button') == "mrz-text":
+            text = request.form.get('mrz-text')
+            print(text)
+            # TODO: Encrypt MRZ and add to DB
+            return f"<h1>Your data has been submitted!</h1>"
     return render_template('upload.html')
